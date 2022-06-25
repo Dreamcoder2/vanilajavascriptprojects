6ccf8057d7a5d824a8ef799da19c7342dce319ac
Dreamcoder2 <106943658+Dreamcoder2@users.noreply.github.com>
2022-06-25 08:10:35 +0530

    Add files via upload

---
+++ 55.website preloader/script.js
@@ -0,0 +1,50 @@
+const typedword = document.querySelector(".typed-word");
+const cursor = document.querySelector(".cursor");
+
+const wordarray = [
+  "MERN Stack Developer.",
+  " UI Designer.",
+  "Creative Programmer.",
+];
+let wordarrayIndex = 0;
+let letterIndex = 0;
+
+const typingdelay = 200;
+const erasingdelay = 100;
+const newworddelay = 2000;
+
+//typing function
+function type() {
+  if (letterIndex < wordarray[wordarrayIndex].length) {
+    typedword.textContent += wordarray[wordarrayIndex].charAt(letterIndex);
+    letterIndex++;
+    setTimeout(type, typingdelay);
+  } else {
+    setTimeout(erase, newworddelay);
+  }
+}
+// Erase function
+function erase() {
+  if (letterIndex > 0) {
+    typedword.textContent = wordarray[wordarrayIndex].substring(
+      0,
+      letterIndex - 1
+    );
+    letterIndex--;
+    setTimeout(erase, erasingdelay);
+  } else {
+    wordarrayIndex++;
+    if (wordarrayIndex >= wordarray.length) {
+      wordarrayIndex = 0;
+    }
+    setTimeout(type, typingdelay);
+  }
+}
+
+type();
+
+//preloader js
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", () => {
+  preloader.style.display = "none";
+});
